a555054cba2e643478c8bb70c88705e76b2ec179
Ankit <ankitraut0987@gmail.com>
2021-09-23 23:41:00 +0530

    Syntax change

--- app.py
+++ app.py
@@ -11,24 +11,46 @@ conn = mysql.connector.connect(
 
 
 @app.route("/")
-def hello_world():
-    try:
-        dict1 = {}
-        mycursor = conn.cursor()
-        mycursor.execute("SELECT * FROM data_table WHERE user_name=%s", (request.args.get("name"),))
-        data = mycursor.fetchall()
-        for row in range(len(data)):
-            temp = data[row]
-            dict1[row] = {"user_name": temp[0],
-                          "site_name": temp[1],
-                          "site_id_name": temp[2],
-                          "site_password": temp[3],
-                          "id_number": temp[4],
-                          }
-        return make_response(jsonify(dict1))
+def default():
+    msghelp = {"Available End Points are: ": ["/ping",
+                                              "",
+                                              """/register-user?name=<Enter Your Name>&ph_no=<Enter Your Phone 
+                                              Number> &mail_id=<Enter Your Mail_Id>&user_id=<Enter the user id 
+                                              you want> &password=<Your Password>&c_password=<Confirm Password>""",
+                                              "",
+                                              "/login?user_id=<Your User Id>&password=<Your Password>",
+                                              "",
+                                              "/getdata?uname=<Your User Name>",
+                                              "",
+                                              """/updatedata?site_u_name=<Your Website User Name>&u_pass=<Your 
+                                              Site Password>&u_name=<Your MRG User Name>&site_name=<Which 
+                                              Web-Site You Want To Update>&id_num=<Website Id Number> """,
+                                              "",
+                                              """/deletedata?u_name=<Your MRG User Name>&site_name=<Which 
+                                              Web-Site You Want To Delete>&id_num=<Website Id Number> """
+                                              ]}
 
-    except Exception as e:
-        print(e)
+    return jsonify(msghelp)
+
+
+# def hello_world():
+#     try:
+#         dict1 = {}
+#         mycursor = conn.cursor()
+#         mycursor.execute("SELECT * FROM data_table WHERE user_name=%s", (request.args.get("name"),))
+#         data = mycursor.fetchall()
+#         for row in range(len(data)):
+#             temp = data[row]
+#             dict1[row] = {"user_name": temp[0],
+#                           "site_name": temp[1],
+#                           "site_id_name": temp[2],
+#                           "site_password": temp[3],
+#                           "id_number": temp[4],
+#                           }
+#         return make_response(jsonify(dict1))
+#
+#     except Exception as e:
+#         print(e)
 
 
 if __name__ == "__main__":
