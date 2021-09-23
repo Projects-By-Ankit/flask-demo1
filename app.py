6a70aa0b53a5eb2c0f71ff25038297b366db2e95
Ankit <ankitraut0987@gmail.com>
2021-09-23 18:36:39 +0530

    Syntax change

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify, request, make_response
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route("/")
-def hello_world():
-    return "Hello and Welcome to Flask App"
+class Help(Resource):
+    def get(self):
+        msg = {"Status": "Alive"}
+        return make_response(jsonify(msg, 200))
 
 
-if __name__ == "__main__":
-    app.run(debug=False,host="0.0.0.0")
+#
+# @app.route("/")
+# def hello_world():
+#     return "Hello and Welcome to Flask App"
+
+api.add_resource(Help, "/")
+
+# if __name__ == "__main__":
+app.run(debug=False, host="0.0.0.0")
