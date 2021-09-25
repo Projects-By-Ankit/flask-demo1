1308ba34f81fa8a3bfc5db6272255eeeca0fefb3
Ankit <ankitraut0987@gmail.com>
2021-09-25 14:21:33 +0530

    Syntax change

--- app.py
+++ app.py
@@ -33,25 +33,5 @@ def default():
     return jsonify(msghelp)
 
 
-# def hello_world():
-#     try:
-#         dict1 = {}
-#         mycursor = conn.cursor()
-#         mycursor.execute("SELECT * FROM data_table WHERE user_name=%s", (request.args.get("name"),))
-#         data = mycursor.fetchall()
-#         for row in range(len(data)):
-#             temp = data[row]
-#             dict1[row] = {"user_name": temp[0],
-#                           "site_name": temp[1],
-#                           "site_id_name": temp[2],
-#                           "site_password": temp[3],
-#                           "id_number": temp[4],
-#                           }
-#         return make_response(jsonify(dict1))
-#
-#     except Exception as e:
-#         print(e)
-
-
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0")
